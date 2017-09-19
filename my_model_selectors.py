5d43120f8a2698fbb2854c9a52a2c6b04d2afa16
Gubbala <gangadhara-naga-sai.gubbala@capgemini.com>
2017-09-19 23:34:23 +0530

    going

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -99,11 +99,22 @@ class SelectorDIC(ModelSelector):
 
 class SelectorCV(ModelSelector):
     ''' select best model based on average log Likelihood of cross-validation folds
-
+        n_splits : int, default=3
+        Number of folds. Must be at least 2.
     '''
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection using CV
+        X = np.array([[1, 2], [3, 4], [1, 2], [3, 4]])
+        y = np.array([1, 2, 3, 4])
+        kf = KFold(n_splits=2)
+        kf.get_n_splits(X)
+
+        print(kf)  
+
+        for train_index, test_index in kf.split(X):
+           print("TRAIN:", train_index, "TEST:", test_index)
+           X_train, X_test = X[train_index], X[test_index]
+           y_train, y_test = y[train_index], y[test_index]
         raise NotImplementedError
